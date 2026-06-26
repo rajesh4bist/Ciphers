2d23efada71c285bc5d0e59b6dd661fb48be5f37
Rajesh Bista <rajesh4bist@gmail.com>
2026-06-26 12:13:26 +0545

    encryption

--- Elgamal.js
+++ Elgamal.js
@@ -1,10 +1,23 @@
 console.log("Elgamal CryptoSystem...");
+
+// let plaintext = document.getElementById("inputbox").value;
+
 const p = 7;
 const g = 5;
 let x = 5;
 
-const h = (Math.pow(g,x)) % 7;
+const h = (Math.pow(g, x)) % 7;
 
 console.log(`Private key: ${x}`);
-console.log(`Public key: ${g,h,p}`);
+console.log(`Public key: ${g, h, p}`);
+
+//Encryption
+let k = 3;
+const C1 = (Math.pow(g, k)) % p;
+const C2 = (m * (Math.pow(h, k))) % p;
+
+console.log(C1,C2);
+
+//Decryption
+
 
